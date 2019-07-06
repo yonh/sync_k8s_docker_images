746ef3b9924f5695b61ada2bce9e676756bf3a6f
yonh lai <azssjli@163.com>
2019-07-06 22:24:24 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # sync_k8s_docker_images
-同步k8s的docker镜像
+同步 `k8s` 的 `docker` 镜像到 `Docker Hub`，也可以直接跳转到我的镜像中心仓库, [点击这里跳转](https://hub.docker.com/u/yonh)
+
+
+
+### 计划工作（如果有时间的话）
+
+- [x] 简单实现同步镜像到Docker Hub
+- [ ] 添加`TravisCI`的支持以实现不用服务器就能同步
+- [ ] 支持国内各大厂的镜像中心上传
+
+### 运行效果
+
+![main](main.png)
---
+++ main.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+set -e
+
+#### version 0.0.1(190706)
+
+#### 脚本作用
+## 本脚本是用来同步使用kubeadm 安装k8s 时需要的镜像（使得国内可以免翻墙使用 kubeadm 安装 k8s）, 嫌麻烦可以使用我的 docker hub 同步的镜像，不必自己同步
+
+#### 软件依赖
+# bash,docker,jq
+# apt update && apt install -y docker.io jq
+
+#### 使用方式
+## 修改本脚本下的 Docker Hub 的账户密码，或设置环境变量 DOCKER_USERNAME, DOCKER_PASSWORD
+## bash main.sh
+
+
+# 由于脚本预计跑在 TravisCi,所以密码使用环境变量配置起来，如果是泡在自己的服务器，可以把注释解开
+DOCKER_USERNAME=""
+DOCKER_PASSWORD=""
+# echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+require_cmds="docker jq"
+for cmd in $require_cmds ; do
+  command -v $cmd >/dev/null 2>&1 || { echo "找不到命令\"$cmd\", 请检查依赖软件是否安装" >&2; exit 1; }
+done
+
+# 需要同步的镜像
+images=(
+k8s.gcr.io/kube-apiserver
+k8s.gcr.io/kube-controller-manager
+k8s.gcr.io/kube-scheduler
+k8s.gcr.io/kube-proxy
+k8s.gcr.io/pause
+k8s.gcr.io/etcd
+k8s.gcr.io/coredns
+k8s.gcr.io/kubernetes-dashboard
+)
+
+mkdir -p k8s.gcr.io
+mkdir -p hub.docker.com
+for image in ${images[@]} ; do
+	# 获取image name,删掉 k8s.gcr.io/ 部分
+    image_name=${image:11}
+
+    #### 下载镜像信息描述文件 ##
+    image_json_file="k8s.gcr.io/${image_name}.json"
+    curl -s -o $image_json_file https://gcr.io/v2/google-containers/${image_name}/tags/list
+    cat ${image_json_file}|jq -e . >/dev/null # 检查 json格式是否正常
+
+    image_json_file="hub.docker.com/${image_name}.json"
+    curl -s -o ${image_json_file} https://hub.docker.com/v2/repositories/${DOCKER_USERNAME}/${image_name}/tags/
+    # 检查 json格式是否正常
+    cat ${image_json_file}|jq -e . >/dev/null # 检查 json格式是否正常
+    #### 下载镜像信息描述文件 [END]###
+
+
+    #### 找出未上传的镜像 ##
+    ## 找出符合的版本号的tag,这里不同步其他零散的包，只同步主版本号的包,如需其他，请修改匹配正则表达式
+    k8s_image_json_file="k8s.gcr.io/${image_name}.json"
+    hub_image_json_file="hub.docker.com/${image_name}.json"
+    
+    regex='^(v?[\.0-9]+)$'
+    for tag in `cat ${k8s_image_json_file}|jq -r '.tags|.[]'`;
+    do
+    	# $tag => k8s tag
+      if [[ $tag =~ $regex ]]; then
+	    #echo $image_name/"${BASH_REMATCH[1]}"
+	    v=${BASH_REMATCH[1]}
+	    
+	    # 取出没有上传到 docker hub 的镜像 tags
+	    exists=`cat hub.docker.com/${image_name}.json|jq -r ".results[]|.name|select(. == \"$v\")|."`
+	    if [[ "$exists" == "" ]]; then
+	    	docker pull $image:$tag
+	    	docker tag $image:$tag ${DOCKER_USERNAME}/${image_name}:${tag}
+	    	docker push ${DOCKER_USERNAME}/${image_name}:${tag}
+	    	docker rmi ${DOCKER_USERNAME}/${image_name}:${tag} $image:$tag
+	    fi
+	  fi
+	done
+done
+
